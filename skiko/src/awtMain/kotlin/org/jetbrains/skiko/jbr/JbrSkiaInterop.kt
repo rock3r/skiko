dd2865b8dc32c6dd8cb321c280e8546b16ab41e9
Sebastiano Poggi <poggos@gmail.com>
2026-04-28 22:15:47 +0200

    Require JBR Skia inline image records

--- skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
+++ skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.ConcurrentHashMap
 object JbrSkiaInterop {
     const val FALLBACK_MARKER = "SKIKO_JBR_INTEROP_FALLBACK"
     const val SCOPE_ACQUIRED_MARKER = "SKIKO_JBR_INTEROP_SCOPE_ACQUIRED"
-    private const val EXPECTED_ABI_ID = 12
+    private const val EXPECTED_ABI_ID = 13
     private const val COMMAND_CAP_CLEAR = 1
     private const val COMMAND_CAP_FILL_RECT = 2
     private const val COMMAND_CAP_STROKE_LINE = 4
@@ -24,6 +24,7 @@ object JbrSkiaInterop {
     private const val COMMAND_CAP_BASIC_TRANSFORMS = 2048
     private const val COMMAND_CAP_CLIP_RECT_OP = 4096
     private const val COMMAND_CAP_SAVE_LAYER = 8192
+    private const val COMMAND_CAP_DRAW_IMAGE_ARGB = 16384
     private const val REQUIRED_COMMAND_CAPABILITIES =
         COMMAND_CAP_CLEAR or
             COMMAND_CAP_FILL_RECT or
@@ -38,7 +39,8 @@ object JbrSkiaInterop {
             COMMAND_CAP_STROKE_METADATA or
             COMMAND_CAP_BASIC_TRANSFORMS or
             COMMAND_CAP_CLIP_RECT_OP or
-            COMMAND_CAP_SAVE_LAYER
+            COMMAND_CAP_SAVE_LAYER or
+            COMMAND_CAP_DRAW_IMAGE_ARGB
     private val JBR_SKIA_CLASSES = arrayOf("com.jetbrains.JBRSkia", "com.jetbrains.desktop.JBRSkia")
     private const val JBR_INTERNAL_SERVICE_CLASS = "com.jetbrains.desktop.JBRSkiaService"
     private const val JBR_ACCESSOR_CLASS = "com.jetbrains.JBR"
