0f30037bf7df3fe76a3a02d4a9aad3b4b814b9a3
Sebastiano Poggi <poggos@gmail.com>
2026-04-29 13:48:19 +0200

    Require JBR Skia linear-gradient command ABI

--- skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
+++ skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.ConcurrentHashMap
 object JbrSkiaInterop {
     const val FALLBACK_MARKER = "SKIKO_JBR_INTEROP_FALLBACK"
     const val SCOPE_ACQUIRED_MARKER = "SKIKO_JBR_INTEROP_SCOPE_ACQUIRED"
-    private const val EXPECTED_ABI_ID = 29
+    private const val EXPECTED_ABI_ID = 30
     private const val EXPECTED_ABI_ID_FOR_TEST_PROPERTY = "skiko.jbr.interop.expectedAbiIdForTest"
     private const val REQUIRED_COMMAND_CAPABILITIES_FOR_TEST_PROPERTY =
         "skiko.jbr.interop.requiredCommandCapabilitiesForTest"
@@ -43,6 +43,7 @@ object JbrSkiaInterop {
     private const val COMMAND_CAP_DRAW_PATH = 134217728
     private const val COMMAND_CAP_DRAW_ARC = 268435456
     private const val COMMAND_CAP_DRAW_ROUND_RECT = 536870912
+    private const val COMMAND_CAP_FILL_RECT_LINEAR_GRADIENT = 1073741824
     private const val REQUIRED_COMMAND_CAPABILITIES =
         COMMAND_CAP_CLEAR or
             COMMAND_CAP_FILL_RECT or
@@ -73,7 +74,8 @@ object JbrSkiaInterop {
             COMMAND_CAP_CLIP_PATH or
             COMMAND_CAP_DRAW_PATH or
             COMMAND_CAP_DRAW_ARC or
-            COMMAND_CAP_DRAW_ROUND_RECT
+            COMMAND_CAP_DRAW_ROUND_RECT or
+            COMMAND_CAP_FILL_RECT_LINEAR_GRADIENT
     private val JBR_SKIA_CLASSES = arrayOf("com.jetbrains.JBRSkia", "com.jetbrains.desktop.JBRSkia")
     private const val JBR_INTERNAL_SERVICE_CLASS = "com.jetbrains.desktop.JBRSkiaService"
     private const val JBR_ACCESSOR_CLASS = "com.jetbrains.JBR"
