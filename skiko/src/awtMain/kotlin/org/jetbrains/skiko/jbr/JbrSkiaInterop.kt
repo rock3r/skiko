dbb664e5114abc8e2d228622f870c6b38d9a46e9
Sebastiano Poggi <poggos@gmail.com>
2026-04-29 09:04:31 +0200

    Require JBR Skia paragraph layout ABI

--- skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
+++ skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.ConcurrentHashMap
 object JbrSkiaInterop {
     const val FALLBACK_MARKER = "SKIKO_JBR_INTEROP_FALLBACK"
     const val SCOPE_ACQUIRED_MARKER = "SKIKO_JBR_INTEROP_SCOPE_ACQUIRED"
-    private const val EXPECTED_ABI_ID = 19
+    private const val EXPECTED_ABI_ID = 20
     private const val COMMAND_CAP_CLEAR = 1
     private const val COMMAND_CAP_FILL_RECT = 2
     private const val COMMAND_CAP_STROKE_LINE = 4
@@ -30,6 +30,7 @@ object JbrSkiaInterop {
     private const val COMMAND_CAP_CLEAR_IMAGE_CACHE = 131072
     private const val COMMAND_CAP_DRAW_PARAGRAPH_UTF16 = 262144
     private const val COMMAND_CAP_PARAGRAPH_FONT_STYLE = 524288
+    private const val COMMAND_CAP_PARAGRAPH_LAYOUT = 1048576
     private const val REQUIRED_COMMAND_CAPABILITIES =
         COMMAND_CAP_CLEAR or
             COMMAND_CAP_FILL_RECT or
@@ -50,7 +51,8 @@ object JbrSkiaInterop {
             COMMAND_CAP_DRAW_TEXT_UTF16 or
             COMMAND_CAP_CLEAR_IMAGE_CACHE or
             COMMAND_CAP_DRAW_PARAGRAPH_UTF16 or
-            COMMAND_CAP_PARAGRAPH_FONT_STYLE
+            COMMAND_CAP_PARAGRAPH_FONT_STYLE or
+            COMMAND_CAP_PARAGRAPH_LAYOUT
     private val JBR_SKIA_CLASSES = arrayOf("com.jetbrains.JBRSkia", "com.jetbrains.desktop.JBRSkia")
     private const val JBR_INTERNAL_SERVICE_CLASS = "com.jetbrains.desktop.JBRSkiaService"
     private const val JBR_ACCESSOR_CLASS = "com.jetbrains.JBR"
