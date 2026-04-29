4385c2774b8537ac5b67464853b46af0988c1fa8
Sebastiano Poggi <poggos@gmail.com>
2026-04-29 11:02:30 +0200

    Require JBR Skia clipPath command ABI

--- skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
+++ skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.ConcurrentHashMap
 object JbrSkiaInterop {
     const val FALLBACK_MARKER = "SKIKO_JBR_INTEROP_FALLBACK"
     const val SCOPE_ACQUIRED_MARKER = "SKIKO_JBR_INTEROP_SCOPE_ACQUIRED"
-    private const val EXPECTED_ABI_ID = 25
+    private const val EXPECTED_ABI_ID = 26
     private const val EXPECTED_ABI_ID_FOR_TEST_PROPERTY = "skiko.jbr.interop.expectedAbiIdForTest"
     private const val REQUIRED_COMMAND_CAPABILITIES_FOR_TEST_PROPERTY =
         "skiko.jbr.interop.requiredCommandCapabilitiesForTest"
@@ -39,6 +39,7 @@ object JbrSkiaInterop {
     private const val COMMAND_CAP_PARAGRAPH_DECORATION = 8388608
     private const val COMMAND_CAP_PARAGRAPH_LETTER_SPACING = 16777216
     private const val COMMAND_CAP_PARAGRAPH_BACKGROUND = 33554432
+    private const val COMMAND_CAP_CLIP_PATH = 67108864
     private const val REQUIRED_COMMAND_CAPABILITIES =
         COMMAND_CAP_CLEAR or
             COMMAND_CAP_FILL_RECT or
@@ -65,7 +66,8 @@ object JbrSkiaInterop {
             COMMAND_CAP_PARAGRAPH_OVERFLOW or
             COMMAND_CAP_PARAGRAPH_DECORATION or
             COMMAND_CAP_PARAGRAPH_LETTER_SPACING or
-            COMMAND_CAP_PARAGRAPH_BACKGROUND
+            COMMAND_CAP_PARAGRAPH_BACKGROUND or
+            COMMAND_CAP_CLIP_PATH
     private val JBR_SKIA_CLASSES = arrayOf("com.jetbrains.JBRSkia", "com.jetbrains.desktop.JBRSkia")
     private const val JBR_INTERNAL_SERVICE_CLASS = "com.jetbrains.desktop.JBRSkiaService"
     private const val JBR_ACCESSOR_CLASS = "com.jetbrains.JBR"
