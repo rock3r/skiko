4873410b07ca5c347c3f50db5c9fcae3cd78eddd
Sebastiano Poggi <poggos@gmail.com>
2026-04-29 11:22:54 +0200

    Require JBR Skia drawArc command ABI

--- skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
+++ skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.ConcurrentHashMap
 object JbrSkiaInterop {
     const val FALLBACK_MARKER = "SKIKO_JBR_INTEROP_FALLBACK"
     const val SCOPE_ACQUIRED_MARKER = "SKIKO_JBR_INTEROP_SCOPE_ACQUIRED"
-    private const val EXPECTED_ABI_ID = 27
+    private const val EXPECTED_ABI_ID = 28
     private const val EXPECTED_ABI_ID_FOR_TEST_PROPERTY = "skiko.jbr.interop.expectedAbiIdForTest"
     private const val REQUIRED_COMMAND_CAPABILITIES_FOR_TEST_PROPERTY =
         "skiko.jbr.interop.requiredCommandCapabilitiesForTest"
@@ -41,6 +41,7 @@ object JbrSkiaInterop {
     private const val COMMAND_CAP_PARAGRAPH_BACKGROUND = 33554432
     private const val COMMAND_CAP_CLIP_PATH = 67108864
     private const val COMMAND_CAP_DRAW_PATH = 134217728
+    private const val COMMAND_CAP_DRAW_ARC = 268435456
     private const val REQUIRED_COMMAND_CAPABILITIES =
         COMMAND_CAP_CLEAR or
             COMMAND_CAP_FILL_RECT or
@@ -69,7 +70,8 @@ object JbrSkiaInterop {
             COMMAND_CAP_PARAGRAPH_LETTER_SPACING or
             COMMAND_CAP_PARAGRAPH_BACKGROUND or
             COMMAND_CAP_CLIP_PATH or
-            COMMAND_CAP_DRAW_PATH
+            COMMAND_CAP_DRAW_PATH or
+            COMMAND_CAP_DRAW_ARC
     private val JBR_SKIA_CLASSES = arrayOf("com.jetbrains.JBRSkia", "com.jetbrains.desktop.JBRSkia")
     private const val JBR_INTERNAL_SERVICE_CLASS = "com.jetbrains.desktop.JBRSkiaService"
     private const val JBR_ACCESSOR_CLASS = "com.jetbrains.JBR"
