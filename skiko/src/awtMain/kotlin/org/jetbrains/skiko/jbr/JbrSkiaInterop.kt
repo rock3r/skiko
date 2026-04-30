22071ae07be0d2baa39ada85a922c86d28d3c2ce
Sebastiano Poggi <poggos@gmail.com>
2026-04-30 13:46:25 +0200

    Require gradient stroke round rect capability

--- skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
+++ skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.ConcurrentHashMap
 object JbrSkiaInterop {
     const val FALLBACK_MARKER = "SKIKO_JBR_INTEROP_FALLBACK"
     const val SCOPE_ACQUIRED_MARKER = "SKIKO_JBR_INTEROP_SCOPE_ACQUIRED"
-    private const val EXPECTED_ABI_ID = 45
+    private const val EXPECTED_ABI_ID = 46
     private const val EXPECTED_NATIVE_ABI_VERSION = 3
     private const val EXPECTED_ABI_ID_FOR_TEST_PROPERTY = "skiko.jbr.interop.expectedAbiIdForTest"
     private const val EXPECTED_NATIVE_ABI_VERSION_FOR_TEST_PROPERTY =
@@ -59,6 +59,7 @@ object JbrSkiaInterop {
     private const val COMMAND_CAP64_TEXT_FONT_FAMILY = 1099511627776L
     private const val COMMAND_CAP64_FILL_RECT_IMAGE_SHADER = 2199023255552L
     private const val COMMAND_CAP64_STROKE_RECT_LINEAR_GRADIENT = 4398046511104L
+    private const val COMMAND_CAP64_STROKE_ROUND_RECT_LINEAR_GRADIENT = 8796093022208L
     private const val REQUIRED_COMMAND_CAPABILITIES =
         COMMAND_CAP_CLEAR.toLong() or
             COMMAND_CAP_FILL_RECT.toLong() or
@@ -102,7 +103,8 @@ object JbrSkiaInterop {
             COMMAND_CAP64_EVICT_IMAGE_CACHE_KEY or
             COMMAND_CAP64_TEXT_FONT_FAMILY or
             COMMAND_CAP64_FILL_RECT_IMAGE_SHADER or
-            COMMAND_CAP64_STROKE_RECT_LINEAR_GRADIENT
+            COMMAND_CAP64_STROKE_RECT_LINEAR_GRADIENT or
+            COMMAND_CAP64_STROKE_ROUND_RECT_LINEAR_GRADIENT
     private val JBR_SKIA_CLASSES = arrayOf("com.jetbrains.JBRSkia", "com.jetbrains.desktop.JBRSkia")
     private const val JBR_INTERNAL_SERVICE_CLASS = "com.jetbrains.desktop.JBRSkiaService"
     private const val JBR_ACCESSOR_CLASS = "com.jetbrains.JBR"
