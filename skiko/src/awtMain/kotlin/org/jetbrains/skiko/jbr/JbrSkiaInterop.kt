7522591d3f1f1921c002309a6c6a07f92e998183
Sebastiano Poggi <poggos@gmail.com>
2026-04-30 17:44:39 +0200

    Require color filter handle eviction

--- skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
+++ skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.ConcurrentHashMap
 object JbrSkiaInterop {
     const val FALLBACK_MARKER = "SKIKO_JBR_INTEROP_FALLBACK"
     const val SCOPE_ACQUIRED_MARKER = "SKIKO_JBR_INTEROP_SCOPE_ACQUIRED"
-    private const val EXPECTED_ABI_ID = 56
+    private const val EXPECTED_ABI_ID = 57
     private const val EXPECTED_NATIVE_ABI_VERSION = 3
     private const val EXPECTED_ABI_ID_FOR_TEST_PROPERTY = "skiko.jbr.interop.expectedAbiIdForTest"
     private const val EXPECTED_NATIVE_ABI_VERSION_FOR_TEST_PROPERTY =
@@ -71,6 +71,7 @@ object JbrSkiaInterop {
     private const val COMMAND_CAP64_DRAW_IMAGE_REF_COLOR_FILTER = 4503599627370496L
     private const val COMMAND_CAP64_DEFINE_COLOR_FILTER_TINT = 9007199254740992L
     private const val COMMAND_CAP64_FILL_RECT_COLOR_FILTER_REF = 18014398509481984L
+    private const val COMMAND_CAP64_EVICT_COLOR_FILTER_HANDLE = 36028797018963968L
     private const val REQUIRED_COMMAND_CAPABILITIES =
         COMMAND_CAP_CLEAR.toLong() or
             COMMAND_CAP_FILL_RECT.toLong() or
@@ -126,7 +127,8 @@ object JbrSkiaInterop {
             COMMAND_CAP64_SAVE_LAYER_COLOR_FILTER or
             COMMAND_CAP64_DRAW_IMAGE_REF_COLOR_FILTER or
             COMMAND_CAP64_DEFINE_COLOR_FILTER_TINT or
-            COMMAND_CAP64_FILL_RECT_COLOR_FILTER_REF
+            COMMAND_CAP64_FILL_RECT_COLOR_FILTER_REF or
+            COMMAND_CAP64_EVICT_COLOR_FILTER_HANDLE
     private val JBR_SKIA_CLASSES = arrayOf("com.jetbrains.JBRSkia", "com.jetbrains.desktop.JBRSkia")
     private const val JBR_INTERNAL_SERVICE_CLASS = "com.jetbrains.desktop.JBRSkiaService"
     private const val JBR_ACCESSOR_CLASS = "com.jetbrains.JBR"
