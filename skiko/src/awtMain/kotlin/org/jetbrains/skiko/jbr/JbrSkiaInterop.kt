dc761fe32e8d2cc8a868b38c1b2d3e0e7bc71073
Sebastiano Poggi <poggos@gmail.com>
2026-05-01 02:09:23 +0200

    Require offset image filter descriptor capability

--- skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
+++ skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.ConcurrentHashMap
 object JbrSkiaInterop {
     const val FALLBACK_MARKER = "SKIKO_JBR_INTEROP_FALLBACK"
     const val SCOPE_ACQUIRED_MARKER = "SKIKO_JBR_INTEROP_SCOPE_ACQUIRED"
-    private const val EXPECTED_ABI_ID = 82
+    private const val EXPECTED_ABI_ID = 83
     private const val EXPECTED_NATIVE_ABI_VERSION = 3
     private const val EXPECTED_ABI_ID_FOR_TEST_PROPERTY = "skiko.jbr.interop.expectedAbiIdForTest"
     private const val EXPECTED_NATIVE_ABI_VERSION_FOR_TEST_PROPERTY =
@@ -148,8 +148,10 @@ object JbrSkiaInterop {
             COMMAND_CAP64_DRAW_IMAGE_REF_COLOR_FILTER_REF or
             COMMAND_CAP64_SAVE_LAYER_BLEND_COLOR_FILTER_REF
     private const val COMMAND_CAP64_HIGH_SAVE_LAYER_IMAGE_FILTER_REF = 1L
+    private const val COMMAND_CAP64_HIGH_EFFECT_DESCRIPTOR_OFFSET_IMAGE_FILTER = 2L
     private const val REQUIRED_COMMAND_CAPABILITIES_HIGH =
-        COMMAND_CAP64_HIGH_SAVE_LAYER_IMAGE_FILTER_REF
+        COMMAND_CAP64_HIGH_SAVE_LAYER_IMAGE_FILTER_REF or
+            COMMAND_CAP64_HIGH_EFFECT_DESCRIPTOR_OFFSET_IMAGE_FILTER
     private val JBR_SKIA_CLASSES = arrayOf("com.jetbrains.JBRSkia", "com.jetbrains.desktop.JBRSkia")
     private const val JBR_INTERNAL_SERVICE_CLASS = "com.jetbrains.desktop.JBRSkiaService"
     private const val JBR_ACCESSOR_CLASS = "com.jetbrains.JBR"
