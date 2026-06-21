e38ac9aab845cc873abf535968d35946902c142d
Sebastiano Poggi <poggos@gmail.com>
2026-06-22 00:31:48 +0200

    Require compact full image ref interop

--- skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
+++ skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInterop.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.ConcurrentHashMap
 object JbrSkiaInterop {
     const val FALLBACK_MARKER = "SKIKO_JBR_INTEROP_FALLBACK"
     const val SCOPE_ACQUIRED_MARKER = "SKIKO_JBR_INTEROP_SCOPE_ACQUIRED"
-    private const val EXPECTED_ABI_ID = 109
+    private const val EXPECTED_ABI_ID = 110
     private const val EXPECTED_NATIVE_ABI_VERSION = 3
     private const val EXPECTED_ABI_ID_FOR_TEST_PROPERTY = "skiko.jbr.interop.expectedAbiIdForTest"
     private const val EXPECTED_NATIVE_ABI_VERSION_FOR_TEST_PROPERTY =
@@ -173,6 +173,7 @@ object JbrSkiaInterop {
     private const val COMMAND_CAP64_HIGH_SAVE_TRANSLATE = 8388608L
     private const val COMMAND_CAP64_HIGH_RESTORE_N = 16777216L
     private const val COMMAND_CAP64_HIGH_SAVE_TRANSLATE_LAYER = 33554432L
+    private const val COMMAND_CAP64_HIGH_DRAW_IMAGE_REF_FULL = 67108864L
     private const val REQUIRED_COMMAND_CAPABILITIES_HIGH =
         COMMAND_CAP64_HIGH_SAVE_LAYER_IMAGE_FILTER_REF or
             COMMAND_CAP64_HIGH_EFFECT_DESCRIPTOR_OFFSET_IMAGE_FILTER or
@@ -199,7 +200,8 @@ object JbrSkiaInterop {
             COMMAND_CAP64_HIGH_STROKE_RECT_IMAGE_SHADER or
             COMMAND_CAP64_HIGH_SAVE_TRANSLATE or
             COMMAND_CAP64_HIGH_RESTORE_N or
-            COMMAND_CAP64_HIGH_SAVE_TRANSLATE_LAYER
+            COMMAND_CAP64_HIGH_SAVE_TRANSLATE_LAYER or
+            COMMAND_CAP64_HIGH_DRAW_IMAGE_REF_FULL
     private val JBR_SKIA_CLASSES = arrayOf("com.jetbrains.JBRSkia", "com.jetbrains.desktop.JBRSkia")
     private const val JBR_INTERNAL_SERVICE_CLASS = "com.jetbrains.desktop.JBRSkiaService"
     private const val JBR_ACCESSOR_CLASS = "com.jetbrains.JBR"
