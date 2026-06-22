fa5fb50ee6f064f8543d10a5523b888704eca1ef
Sebastiano Poggi <poggos@gmail.com>
2026-06-22 09:28:56 +0200

    Require full image restore capability

--- skiko/src/awtTest/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInteropTest.kt
+++ skiko/src/awtTest/kotlin/org/jetbrains/skiko/jbr/JbrSkiaInteropTest.kt
@@ -213,6 +213,7 @@ class JbrSkiaInteropTest {
             "full image ref run" to COMMAND_CAP64_HIGH_DRAW_IMAGE_REF_FULL_RUN,
             "stroke line and full image ref run" to COMMAND_CAP64_HIGH_STROKE_LINE_DRAW_IMAGE_REF_FULL_RUN,
             "save translate layer and save translate" to COMMAND_CAP64_HIGH_SAVE_TRANSLATE_LAYER_SAVE_TRANSLATE,
+            "full image ref and restore" to COMMAND_CAP64_HIGH_DRAW_IMAGE_REF_FULL_RESTORE,
         )
 
         capabilities.forEach { (label, capability) ->
@@ -950,7 +951,7 @@ class JbrSkiaInteropTest {
         private const val REQUIRED_COMMAND_CAPABILITIES_WITHOUT_SAVE_LAYER_BLEND_COLOR_FILTER_REF = Long.MAX_VALUE
         private const val REQUIRED_COMMAND_CAPABILITIES =
             -1L
-        private const val REQUIRED_COMMAND_CAPABILITIES_HIGH = 27111981055L
+        private const val REQUIRED_COMMAND_CAPABILITIES_HIGH = 61471719423L
         private const val COMMAND_CAP64_HIGH_SAVE_LAYER_IMAGE_FILTER_REF = 1L
         private const val COMMAND_CAP64_HIGH_EFFECT_DESCRIPTOR_OFFSET_IMAGE_FILTER = 2L
         private const val COMMAND_CAP64_HIGH_EFFECT_DESCRIPTOR_CHAIN_IMAGE_FILTER = 4L
@@ -982,6 +983,7 @@ class JbrSkiaInteropTest {
         private const val COMMAND_CAP64_HIGH_DRAW_IMAGE_REF_FULL_RUN = 1073741824L
         private const val COMMAND_CAP64_HIGH_STROKE_LINE_DRAW_IMAGE_REF_FULL_RUN = 8589934592L
         private const val COMMAND_CAP64_HIGH_SAVE_TRANSLATE_LAYER_SAVE_TRANSLATE = 17179869184L
+        private const val COMMAND_CAP64_HIGH_DRAW_IMAGE_REF_FULL_RESTORE = 34359738368L
         private const val REQUIRED_COMMAND_CAPABILITIES_HIGH_WITHOUT_SHADER_DESCRIPTOR_COLOR_FILTER =
             REQUIRED_COMMAND_CAPABILITIES_HIGH and COMMAND_CAP64_HIGH_SHADER_DESCRIPTOR_COLOR_FILTER.inv()
         private const val REQUIRED_COMMAND_CAPABILITIES_HIGH_WITHOUT_DRAW_POINTS =
