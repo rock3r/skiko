9fe2f5caa33f2ff2d6c04479bea05018674de86c
Sebastiano Poggi <poggos@gmail.com>
2026-06-26 12:44:13 +0200

    Space adaptive cache probes further

--- skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaSwingLayer.kt
+++ skiko/src/awtMain/kotlin/org/jetbrains/skiko/jbr/JbrSkiaSwingLayer.kt
@@ -9968,7 +9968,7 @@ internal class EncodedCommandBufferCache(
         private const val LOG_COMMAND_BUFFER_CACHE_INTERVAL = 120
         private const val MAX_CONSECUTIVE_MISSES_BEFORE_BYPASS = 16
         private const val MAX_CONSECUTIVE_DEFERRALS_BEFORE_BYPASS = 2
-        private const val BYPASS_PROBE_INTERVAL = 240
+        private const val BYPASS_PROBE_INTERVAL = 480
     }
 }
 
